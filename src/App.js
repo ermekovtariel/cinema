4531b962a0a30c9750c9806586afcc22c4d6fd50
ermekovtariel <ermekovtariel@gmail.com>
2021-03-12 14:56:43 +0600

    FFirst commit on cinema

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import axios from "axios";
 function App() {
+
+  
+  const options = {
+    method: 'GET',
+    url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup/',
+    params: {term: 'bojack', country: 'uk'},
+    headers: {
+      'x-rapidapi-key': '8c71aa6288mshf8d721368e32656p1f7f47jsnffd8e8f430e1',
+      'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      123
+
     </div>
   );
 }
